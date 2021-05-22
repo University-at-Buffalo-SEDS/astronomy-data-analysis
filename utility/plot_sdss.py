2b1065f503331eecb47b807c6e530fddb7432e01
k-z-m-r <jdkazime@buffalo.edu>
2021-05-21 23:49:54 -0400

    get color map
    We have a function now to generate the color map.

--- utility/plot_sdss.py
+++ utility/plot_sdss.py
@@ -31,36 +31,59 @@ def get_one_hot(patch_of_sky):
     # Define a numpy array to store the Boolean values.
     one_hot = np.zeros((classes.shape[0], unq_classes.shape[0]), dtype = int)
 
+    # Iterating over to assign the binary values to the one-hot.
     for idx, elem in enumerate(unq_classes):
+
+        # The locations where our current classes is equivalent
+        # to all of the classes of the data.  This is True or False.
         loc = elem == classes
+
         one_hot[:, idx] = loc
 
     return one_hot
 
+# We can use this to generate a colormapping for the celestial objects.
 def get_color_map(one_hot, obj_colors):
+
+    '''
+        one_hot -> the one hot encoding of the data.
+        obj_colors -> the colors by which the data will be mapped.
+    '''
+
+    # Storing the colors.
     obj_cmap = []
 
-    if (type(obj_colors) != str) and (len(obj_colors) != len(unq_classes)):
-        print('The size of the input colors does not match the size of the number of unique classes.\n')
-        print('Returning just the one hot.')
-        return one_hot
+    # Dimensions for iterative purposes.
+    num_classes = one_hot.shape[0]
+    num_unq_classes = one_hot.shape[1]
+
+    # Checking to make sure that the input is the same shape as 
+    # the number of unique classes for indexing purposes.
+    if (type(obj_colors) != str) and (len(obj_colors) != num_unq_classes):
+        print('The size of the input colors does not match the size of the number of unique classes.')
+        return None
 
     else:
-        for idx, _ in enumerate(classes):
-
+        for idx in range(num_classes):
+            
+            # We initialize the class by index.
             obj_cmap.append('')
-            for jdx, _ in enumerate(unq_classes):
+
+            for jdx in range(num_unq_classes):
+
+                # Setting the color for string multiplication.
                 if type(obj_colors) == str:
                     color = obj_colors
                 else:
                     color = obj_colors[jdx]
+                
+                # If one_hot[idx, jdx] is 0, then it appends an empty string.
+                # Otherwise, if it's 1, then it appends the color once.
                 obj_cmap[idx] += one_hot[idx, jdx]*color
 
-    
+    return obj_cmap
 
-
-
-
+# We can use this to plot right ascension and declination.
 def plot_celestial_coordinates(patch_of_sky, obj_size = 100, obj_alpha = 1, obj_cmap = None):
 
     '''
@@ -88,10 +111,5 @@ def plot_celestial_coordinates(patch_of_sky, obj_size = 100, obj_alpha = 1, obj_
     ax.set_title('celestial coordinates of the patch', fontsize = 14)
 
     return fig, ax
-ra = (0, .1)
-dec = ra
-print('red'*0)
-df = get_sky_patch(ra, dec)
 
-get_one_hot(df, ['red', 'blue', 'green'])
 
